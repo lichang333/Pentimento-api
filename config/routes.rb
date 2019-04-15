e5f59e1c0f36f5595f2c60ad035bc605b23082ad
lichang333 <lichang@oregonstate.edu>
2019-04-16 06:29:53 +0800

    update config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/login', to: 'login#login'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :galleries, only: [:index, :show]
-      resources :arts, only: [:index, :show]
+      resources :galleries, only: [ :index, :show] do
+        resources :arts, only: [ :index, :show]
+      end
     end
   end
 end
